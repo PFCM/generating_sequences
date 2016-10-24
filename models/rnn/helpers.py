09ad8fac98d808b4d24f2358b2d3890d6c721f50
Paul Mathews <pfcmathews@gmail.com>
2016-10-24 16:36:12 +1300

    manually collect outputs, turns out tf doesn't for some reason

--- models/rnn/helpers.py
+++ models/rnn/helpers.py
@@ -9,13 +9,15 @@ def _maybe_project(var, proj):
     return var
 
 
-def argmax_and_embed(embedding, output_projection=None):
+def argmax_and_embed(embedding, output_list=None, output_projection=None):
     """Returns a callable (usable as a loop_fn for seq2seq) which takes the
     argmax of a batch of outputs and embeds them. Optionally applies a
     projection first.
 
     Args:
         embedding: an embedding matrix to lookup symbols in.
+        output_list (Optional): list to append all sampled outputs to.
+            Optional, default is not to collect them.
         output_proj (Optional): tuple (weight, biases) used to project outputs.
             If None (default), no projection is performed.
 
@@ -25,12 +27,17 @@ def argmax_and_embed(embedding, output_projection=None):
     def _argmax_embed(prev, i):
         var = _maybe_project(prev, output_projection)
         next_ = tf.nn.embedding_lookup(embedding, tf.argmax(var, 1))
+
+        if output_list is not None:
+            output_list.append(next_)
+
         return next_
 
     return _argmax_embed
 
 
-def sample_and_embed(embedding, temperature, output_projection=None):
+def sample_and_embed(embedding, temperature, output_list,
+                     output_projection=None):
     """Returns a callable (usable as a loop_fn for seq2seq) which takes a
     sample from a batch of outputs and embeds them. Optionally applies a
     projection first.
@@ -38,6 +45,8 @@ def sample_and_embed(embedding, temperature, output_projection=None):
     Args:
         embedding: an embedding matrix to lookup symbols in.
         temperature: temperature to control the pointiness of the softmax.
+        output_list (Optional): a list in which to collect the samples.
+            Default None means don't collect them at all.
         output_proj (Optional): tuple (weight, biases) used to project outputs.
             If None (default), no projection is performed.
 
@@ -49,6 +58,10 @@ def sample_and_embed(embedding, temperature, output_projection=None):
         var /= temperature
         next_ = tf.nn.embedding_lookup(
             embedding, tf.multinomial(var, 1))
+
+        if output_list is not None:
+            output_list.append(next_)
+
         return next_
 
     return _sample_embed
